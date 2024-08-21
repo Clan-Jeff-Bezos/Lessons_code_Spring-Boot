6d28e06af3e3278170281400b68b4f6b868848d1
antony martinez <antonymaruiz02@gmail.com>
2024-08-20 21:03:22 -0500

    Update 20/08/24

---
+++ JPA/src/main/java/com/riwi/RiwiMovies/entities/UserEntity.java
@@ -0,0 +1,67 @@
+package com.riwi.RiwiMovies.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id; // Varchar(255)
+    private String name;
+    private String email;
+    private String password;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String id, String password, String name, String email) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
