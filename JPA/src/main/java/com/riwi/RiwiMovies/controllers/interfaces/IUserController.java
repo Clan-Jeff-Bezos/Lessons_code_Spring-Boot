69e321bcbdd1dd49945d2518a77994b3704ffe8f
antony martinez <antonymaruiz02@gmail.com>
2024-08-26 19:45:19 -0500

    Update

--- JPA/src/main/java/com/riwi/RiwiMovies/controllers/interfaces/IUserController.java
+++
@@ -1,9 +0,0 @@
-package com.riwi.RiwiMovies.controllers.interfaces;
-
-import com.riwi.RiwiMovies.controllers.generic.Create;
-import com.riwi.RiwiMovies.entities.UserEntity;
-
-public interface IUserController extends
-        Create<UserEntity> {
-
-}
